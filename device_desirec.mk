4136dc2156d1d113400f08f7a0c61f471a32330d
bradvbos <stupiddaddy@gmail.com>
2011-03-30 15:00:24 -0400

    update

--- device_desirec.mk
+++ device_desirec.mk
@@ -55,7 +55,6 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
@@ -110,10 +109,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # OpenGL ES 1.1-CM
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version = 65537 \
+    ro.opengles.version = 65536 \
     debug.sf.hw=1 \
-    video.accelerate.hw =1 \
-    debug.performance.tuning =1 \
     ro.product.multi_touch_enabled=true
 
 # Default network type
